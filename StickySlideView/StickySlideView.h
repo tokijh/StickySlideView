219bdd6658179697ed0fad276ca5d57a6b48a9ce
tokijh <tokijh@naver.com>
2018-06-20 11:07:40 +0900

    Initial Commit

---
+++ StickySlideView/StickySlideView.h
@@ -0,0 +1,19 @@
+//
+//  StickySlideView.h
+//  StickySlideView
+//
+//  Created by  Joonghyun-Yoon on 2018. 6. 20..
+//  Copyright © 2018년 tokijh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StickySlideView.
+FOUNDATION_EXPORT double StickySlideViewVersionNumber;
+
+//! Project version string for StickySlideView.
+FOUNDATION_EXPORT const unsigned char StickySlideViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StickySlideView/PublicHeader.h>
+
+
